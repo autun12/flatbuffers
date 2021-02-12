d1938c9d98f3e84e97e1b90fe6984efb6e4bcf94
autun12 <austindrane56@gmail.com>
2021-02-11 20:45:55 -0600

    Added premake file

--- bin-int/Debug-linux-x86_64/Flatbuffers/util.d
+++
@@ -1,6 +0,0 @@
-bin-int/Debug-linux-x86_64/Flatbuffers/util.o: src/util.cpp \
- include/flatbuffers/base.h include/flatbuffers/util.h \
- include/flatbuffers/stl_emulation.h
-include/flatbuffers/base.h:
-include/flatbuffers/util.h:
-include/flatbuffers/stl_emulation.h:
